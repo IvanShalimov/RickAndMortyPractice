5f1cfe446670eefa08a0b3e59128dead25768f1f
Ivan Shalimov <iv.shalimoff@yandex.ru>
2025-06-19 21:55:03 +0300

    Fix detekt + add build health util

---
+++ app/src/main/java/com/djangorussia/rickandmortypractice/Greeting.kt
@@ -0,0 +1,23 @@
+package com.djangorussia.rickandmortypractice
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.djangorussia.rickandmortypractice.ui.theme.RickAndMortyPracticeTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickAndMortyPracticeTheme {
+        Greeting("Android")
+    }
+}
